59ea8132f4013a686e1fdec6dc86633f653b1389
a-y-a-n-das <ayandas8125@gmail.com>
2026-02-18 16:38:58 +0530

    stats endpoint added

--- backend/tickets/urls.py
+++ backend/tickets/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from .views import TicketViewSet
+from django.urls import path
+
 
 router = DefaultRouter()
 router.register(r"tickets", TicketViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls 
--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -3,42 +3,48 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Ticket
 from .serializers import TicketSerializer
+from rest_framework.response import Response
+from django.db.models import Count, Avg
+from django.db.models.functions import TruncDate
+from rest_framework.decorators import action
+
+
 # Create your views here.
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
 
-@api_view(['GET'])
-def ticket_stats(request):
-    total_tickets = Ticket.objects.count()
-    open_tickets = Ticket.objects.filter(status='open').count()
-    
-    priority_data = (
-        Ticket.objects.values('priority').annotate(count=Count('id'))
-    )
-    
-    priority_breakdown = {item['priority']: item['count'] for item in priority_data}
-
-    category_data = (
-        Ticket.objects.values('category').annotate(count=Count('id'))
-    )
-
-    category_breakdown = {item['category']: item['count'] for item in category_data}
-
-    daily_counts = (
-        Ticket.objects.annotate(date=TruncDate('created_at'))
-        .values('date')
-        .annotate(count=Count('id'))
+    @action(detail=False, methods=['get'])
+    def stats(self, request):
+        total_tickets = Ticket.objects.count()
+        open_tickets = Ticket.objects.filter(status='open').count()
         
-    )
+        priority_data = (
+            Ticket.objects.values('priority').annotate(count=Count('id'))
+        )
+        
+        priority_breakdown = {item['priority']: item['count'] for item in priority_data}
 
-    avg_tickets_per_day = daily_counts.aggregate(avg=Avg('count'))['avg'] or 0
+        category_data = (
+            Ticket.objects.values('category').annotate(count=Count('id'))
+        )
 
-    return Response({
-        'total_tickets': total_tickets,
-        'open_tickets': open_tickets,
-        'priority_breakdown': priority_breakdown,
-        'category_breakdown': category_breakdown,
-        'avg_tickets_per_day': avg_tickets_per_day,
-    })
+        category_breakdown = {item['category']: item['count'] for item in category_data}
+
+        daily_counts = (
+            Ticket.objects.annotate(date=TruncDate('created_at'))
+            .values('date')
+            .annotate(count=Count('id'))
+            
+        )
+
+        avg_tickets_per_day = daily_counts.aggregate(avg=Avg('count'))['avg'] or 0
+
+        return Response({
+            'total_tickets': total_tickets,
+            'open_tickets': open_tickets,
+            'priority_breakdown': priority_breakdown,
+            'category_breakdown': category_breakdown,
+            'avg_tickets_per_day': avg_tickets_per_day,
+        })
